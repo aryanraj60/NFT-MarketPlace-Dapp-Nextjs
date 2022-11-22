5f029804120471de3eb2c1fe6c110b640cda7670
Aryan Raj <aryanraj234.ar@gmail.com>
2022-11-22 16:50:10 +0530

    final changes to footer

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,11 +4,11 @@ import { SiTwitter, SiLinkedin } from "react-icons/si";
 const Footer = () => {
   return (
     <div>
-      <div class="mt-5 text-center text-white">
-        <div class="container p-3">
-          <section class="mb-4">
+      <div className="mt-5 text-center text-white">
+        <div className="container p-3">
+          <section className="mb-4">
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="https://twitter.com/aryanraj_60"
               src="../../assests/twitter.png"
             >
@@ -16,7 +16,7 @@ const Footer = () => {
             </a>
 
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               src="../../assests/linkedin.png"
               href="https://www.linkedin.com/in/aryan-rajput-3a0760204/"
             >
@@ -24,7 +24,7 @@ const Footer = () => {
             </a>
 
             <a
-              class="btn btn-outline-light btn-floating m-1"
+              className="btn btn-outline-light btn-floating m-1"
               href="https://goerli.etherscan.io/address/0x26B2F09c2e10Dc56fe48fbc69d667207Ec92E48E"
               role="button"
             >
